cd509040048d499e33dd2c0070471f2149a27e17
Alex Koenig <alex.koenig4@gmail.com>
2020-02-03 23:04:27 -0500

    Add clear and new grid buttons
    Added a 'clear' button which resets the entire grid to a white
    background. Added a 'new grid' button which allows the user to input
    the size of the grid they want and creates an X-by-X grid based on the
    number input by the user. The new grid fits in the same 640px by 640px
    container regardless of size.

--- script.js
+++ script.js
@@ -1,24 +1,38 @@
 $(document).ready(function () {
     const gridContainer = document.getElementById("gridContainer");
     let div
+    let gridSize
 
-    for(row = 1; row <= 16; row++) {
-        for(col = 1; col <= 16; col++){
-            div = document.createElement("div");
-            div.className = "gridBox";
-            div.style.gridColumnStart = col;
-            div.style.gridColumnEnd = (col + 1);
-            div.style.gridRowStart = row;
-            div.style.gridRowEnd = (row + 1);
-            gridContainer.appendChild(div);
+    const newGridButton = document.getElementById("newGrid");
+    newGridButton.addEventListener('click', () => {
+        gridSize = prompt("Enter New Grid Size (e.g. 16 creates a 16x16 grid):")
+        gridContainer.style.gridTemplateColumns = `repeat(${gridSize}, ${640/gridSize}px)`;
+        gridContainer.style.gridTemplateRows = `repeat(${gridSize}, ${640/gridSize}px)`;
+        for(row = 1; row <= gridSize; row++) {
+            for(col = 1; col <= gridSize; col++){
+                div = document.createElement("div");
+                div.className = "gridBox";
+                div.style.gridColumnStart = col;
+                div.style.gridColumnEnd = (col + 1);
+                div.style.gridRowStart = row;
+                div.style.gridRowEnd = (row + 1);
+                div.style.backgroundColor = 'white';
+                gridContainer.appendChild(div);
+            }
         }
-    }
 
-    const gridBoxes = document.querySelectorAll('.gridBox')
-    gridBoxes.forEach((div) => {
-        div.addEventListener('mouseover', () => {
-            div.style.backgroundColor = 'black';
+        const gridBoxes = document.querySelectorAll('.gridBox')
+        gridBoxes.forEach((div) => {
+            div.addEventListener('mouseover', () => {
+                div.style.backgroundColor = 'black';
+            })
+        })
+
+        const clearButton = document.getElementById("clearButton");
+        clearButton.addEventListener('click', () => {
+            gridBoxes.forEach((div) => {
+                div.style.backgroundColor = 'white';
+            })
         })
     })
-
 })
