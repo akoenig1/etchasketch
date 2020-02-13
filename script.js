162d745f1bd0575cddf94b8eb664e0a09819d50e
Alex Koenig <alex.koenig4@gmail.com>
2020-02-13 00:41:21 -0500

    Fix grid resize bugs
    Fixed various bugs associated with resizing the grid.
    
    Moved all event listeners inside the resize grid button event listener
    so that they still listen after the user presses the resize grid button.
    
    Added code to remove all existing divs from grid container when grid
    is resized. This bug was causing display issues due to lingering
    properties of pre-existing grid elements. New grid elements are only
    created once old grid elements have been removed.
    
    Added highlightColor function to bring most recently selected color
    back into focus after either the clear grid or resize grid buttons
    is pressed.

--- script.js
+++ script.js
@@ -11,86 +11,113 @@ $(document).ready(function () {
     let currentShade
 
     // default to 16x16 grid when the page loads
-    resizeGrid(16);    
+    //resizeGrid(16);    
 
     // create a new grid of the users choosing when the new grid button is pressed
     const newGridButton = document.getElementById("newGrid");
     newGridButton.addEventListener('click', () => {
         resizeGrid(prompt("Enter Grid Size (e.g. 16 creates a 16x16 grid):"));
-    })
 
-    // clear the entire grid back to all white boxes
-    const clearButton = document.getElementById("clearButton");
-    clearButton.addEventListener('click', () => {
-        gridBoxes.forEach((div) => {
-            div.style.backgroundColor = 'white';
+        // clear the entire grid back to all white boxes
+        const clearButton = document.getElementById("clearButton");
+        clearButton.addEventListener('click', () => {
+            gridBoxes.forEach((div) => {
+                div.style.backgroundColor = 'white';
+            })
+            // show user which drawing color they have selected
+            highlightColor(color);
         })
-    })
 
-    // change selected color to black
-    const gridBlack = document.getElementById('gridBlack');
-    gridBlack.addEventListener('click', () => {
-        color = 'black';
-    })
+        // change selected color to black
+        const gridBlack = document.getElementById('gridBlack');
+        gridBlack.addEventListener('click', () => {
+            color = 'black';
+        })
 
-    // change selected color to white
-    const gridEraser = document.getElementById('gridEraser');
-    gridEraser.addEventListener('click', () => {
-        color = 'white';
-    })
+        // change selected color to white
+        const gridEraser = document.getElementById('gridEraser');
+        gridEraser.addEventListener('click', () => {
+            color = 'white';
+        })
 
-    // change selected color to randomly generated colors
-    const gridRandom = document.getElementById('gridRandom');
-    gridRandom.addEventListener('click', () => {
-        color = 'random';
-    })
+        // change selected color to randomly generated colors
+        const gridRandom = document.getElementById('gridRandom');
+        gridRandom.addEventListener('click', () => {
+            color = 'random';
+        })
 
-    // change selected color to grayscale
-    const gridGrayscale = document.getElementById('gridGrayscale');
-    gridGrayscale.addEventListener('click', () => {
-        color = 'grayscale';
-    })
+        // change selected color to grayscale
+        const gridGrayscale = document.getElementById('gridGrayscale');
+        gridGrayscale.addEventListener('click', () => {
+            color = 'grayscale';
+        })
 
-    // change hovered-over grid boxes to selected color
-    const gridBoxes = document.querySelectorAll('.gridBox')
-    gridBoxes.forEach((div) => {
-        div.addEventListener('mouseover', () => {
-            if(color === 'random') {
-                red = Math.random() * 255;
-                green = Math.random() * 255;
-                blue = Math.random() * 255;
-                div.style.backgroundColor = `rgb(${red}, ${green}, ${blue})`
-            }
-            else if(color === 'grayscale') {
-                currentBackground = $(div).css('background-color');
-                currentShade = currentBackground.split(",");
-                if(currentBackground === 'rgb(0, 0, 0)'){
-                    opacity = 1;
+        // change hovered-over grid boxes to selected color
+        const gridBoxes = document.querySelectorAll('.gridBox')
+        gridBoxes.forEach((div) => {
+            div.addEventListener('mouseover', () => {
+                if(color === 'random') {
+                    red = Math.random() * 255;
+                    green = Math.random() * 255;
+                    blue = Math.random() * 255;
+                    div.style.backgroundColor = `rgb(${red}, ${green}, ${blue})`
                 }
-                else if(!(currentShade[3])){
-                    opacity = 0.1;
-                }
-                else {
-                    currentShade = Number(currentShade[3].replace(')', ''));
-                    if(currentShade === 1) {
+                else if(color === 'grayscale') {
+                    currentBackground = $(div).css('background-color');
+                    console.log(currentBackground);
+                    currentShade = currentBackground.split(",");
+                    if(currentBackground === 'rgb(0, 0, 0)'){
                         opacity = 1;
                     }
-                    else{
-                        opacity = currentShade + 0.1;
+                    else if(!(currentShade[3])){
+                        opacity = 0.1;
+                        div.style.backgroundColor = `rgba(0, 0, 0, ${opacity})`;
+                    }
+                    else {
+                        currentShade = Number(currentShade[3].replace(')', ''));
+                        if(currentShade === 1) {
+                            opacity = 1;
+                            div.style.backgroundColor = `rgb(0, 0, 0)`;
+                        }
+                        else{
+                            opacity = currentShade + 0.1;
+                            div.style.backgroundColor = `rgba(0, 0, 0, ${opacity})`;
+                        }
                     }
                 }
-                div.style.backgroundColor = `rgba(0, 0, 0, ${opacity})`;
-            }
-            else {
-                div.style.backgroundColor = color;
-            }
+                else {
+                    div.style.backgroundColor = color;
+                }
+            })
         })
+        highlightColor(color);
     })
+
+    // show user which drawing color they have selected
+    function highlightColor(color){
+        switch(color) {
+            case 'black':
+                gridBlack.focus();
+                break;
+            case 'white':
+                gridEraser.focus();
+                break;
+            case 'random':
+                gridRandom.focus();
+                break;
+            case 'grayscale':
+                gridGrayscale.focus();
+                break;
+        }
+    }
 
     // create an X-by-X size grid of divs within a 640px-by-640px container div
     function resizeGrid(gridSize){
-        gridContainer.style.gridTemplateColumns = `repeat(${gridSize}, ${640/gridSize}px)`;
-        gridContainer.style.gridTemplateRows = `repeat(${gridSize}, ${640/gridSize}px)`;
+        while(gridContainer.firstChild) {
+            gridContainer.removeChild(gridContainer.firstChild);
+        }
+        gridContainer.style.gridTemplateColumns = `repeat(${gridSize}, ${600/gridSize}px)`;
+        gridContainer.style.gridTemplateRows = `repeat(${gridSize}, ${600/gridSize}px)`;
         for(row = 1; row <= gridSize; row++) {
             for(col = 1; col <= gridSize; col++){
                 div = document.createElement("div");
@@ -99,7 +126,7 @@ $(document).ready(function () {
                 div.style.gridColumnEnd = (col + 1);
                 div.style.gridRowStart = row;
                 div.style.gridRowEnd = (row + 1);
-                div.style.backgroundColor = 'white';
+                div.style.backgroundColor = 'rgb(255, 255, 255)';
                 gridContainer.appendChild(div);
             }
         }
